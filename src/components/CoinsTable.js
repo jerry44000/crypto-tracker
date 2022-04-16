927ae6f983cd84e47a292b3ffe2aa95fde796431
jerry44000 <shai.badarna@gmail.com>
2022-04-16 18:45:30 +0200

    coins table & tablerow & tablecell

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CoinList } from "../config/api.js";
 import { CryptoState } from "../context/CryptoContext.js";
+import Pagination from "@material-ui/lab/Pagination";
 import {
   makeStyles,
   createTheme,
@@ -12,12 +13,22 @@ import {
   Container,
   TextField,
   InputAdornment,
+  TableContainer,
+  TableCell,
+  TableHead,
+  Table,
+  TableBody,
+  TableRow,
+  LinearProgress,
 } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
+import { useNavigate } from "react-router-dom";
 
 const CoinsTable = () => {
+  let navigate = useNavigate();
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState();
 
   const { currency } = CryptoState();
 
@@ -40,6 +51,30 @@ const CoinsTable = () => {
       type: "dark",
     },
   });
+
+  const handleSearch = () => {
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(search) ||
+        coin.symbol.toLowerCase().includes(search)
+    );
+  };
+  const useStyles = makeStyles({
+    row: {
+      backgroundColor: "#16171a",
+      cursor: "pointer",
+      "&:hover": {
+        backgroundColor: "#131111",
+      },
+      fontFamily: "Montserrat",
+    },
+    pagination: {
+      "& .MuiPaginationItem-root": {
+        color: "gold",
+      },
+    },
+  });
+  const classes = useStyles();
   return (
     <ThemeProvider theme={darkTheme}>
       <Container style={{ textAlign: "center" }}>
@@ -50,6 +85,7 @@ const CoinsTable = () => {
           variant="outlined"
           label="Search"
           style={{ marginBottom: 20, width: "50%" }}
+          onChange={(e) => setSearch(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -57,7 +93,50 @@ const CoinsTable = () => {
               </InputAdornment>
             ),
           }}
-        ></TextField>
+        />
+        <TableContainer>
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: "green" }} />
+          ) : (
+            <Table>
+              <TableHead style={{ backgroundColor: "green" }}>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Oswald",
+                      }}
+                      key={head}
+                        align={head === "Coin" ? "left" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {handleSearch().map((row) => {
+                  const profit = row.price_change_percentage_24h > 0;
+                  return (
+                    <TableRow
+                      onClick={() => navigate(`/coins/${row.id}`)}
+                      key={row.name}
+                      className={classes.row}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        style={{ display: "flex", gap: 15 }}
+                      ></TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
+        </TableContainer>
       </Container>
     </ThemeProvider>
   );
