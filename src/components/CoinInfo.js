a4d2b1d012f0247ac8854b0035119d0e4b2c9575
jerry44000 <shai.badarna@gmail.com>
2022-04-17 15:14:20 +0200

    coin page & coin info

---
+++ src/components/CoinInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinInfo = () => {
+    return (
+        <div>
+            Coin Info
+        </div>
+    );
+};
+
+export default CoinInfo;
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api.js";
+import { CryptoState } from "../context/CryptoContext.js";
+import CoinInfo from "../components/CoinInfo.js";
+import {
+  makeStyles,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 
 const CoinPage = () => {
-    return (
-        <div>
-            Coin Page
-        </div>
-    );
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "flex",
+      [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+    },
+    sideBar: {
+      width: "30%",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: 25,
+      borderRight: "2px solid grey",
+    },
+    heading: {
+        fontWeight: "bold",
+        marginBottom: 20,
+        fontFamily: "Oswald"
+    }
+  }));
+
+  const classes = useStyles();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+  return (
+    <div className={classes.container}>
+      <div className={classes.sideBar}>
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          type={{ marginBottom: 20 }}
+        />
+        <Typography variant="h3" className={classes.heading}>
+            {coin?.name}
+        </Typography>
+        <Typography>
+            {coin?.description.en.split(". ")[0]}
+        </Typography>
+      </div>
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default CoinPage;
